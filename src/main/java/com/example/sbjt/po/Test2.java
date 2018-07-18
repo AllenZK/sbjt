c8af00d39ba706e62f76da1dcf799de201a39402
AllenZK <jclient@163.com>
2018-07-18 14:28:36 +0800

    从test2新建分支test3，创建test2方法

--- src/main/java/com/example/sbjt/po/Test2.java
+++ src/main/java/com/example/sbjt/po/Test2.java
@@ -5,5 +5,12 @@ package com.example.sbjt.po;
  * @description: 测试test2分支
  */
 public class Test2 {
-    
+    public static void main(String[] args) {
+
+        //test2分支添加了main方法
+
+    }
+    public static void test3(){
+        System.out.println("从test2新建分支test3,并添加该方法！");
+    }
 }
