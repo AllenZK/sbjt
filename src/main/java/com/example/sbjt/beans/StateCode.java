cd2f87e4a669d48fd11d04f2222b37c0b6ad3f70
AllenZK <jclient@163.com>
2018-08-08 09:20:54 +0800

    跟新了beans实体

---
+++ src/main/java/com/example/sbjt/beans/StateCode.java
@@ -0,0 +1,38 @@
+package com.example.sbjt.beans;
+
+/**
+ * @auth: Created by zk on 2018/8/8
+ * @description:
+ */
+public class StateCode<T> {
+
+    private String code;
+
+    private String msg;
+
+    private T data;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
